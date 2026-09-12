d87e53485c5f46ea00eb7ac0f366bafd69bb8b80
shreyastouih-ctrl <shreyastouih-ctrl@users.noreply.github.com>
2026-09-12 22:59:38 +0530

    Fix package names for Arch Linux: pcmanfm-gtk3 -> pcmanfm, xwayland -> xorg-xwayland

--- profiledef.sh
+++ profiledef.sh
@@ -21,7 +21,6 @@ airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15')
 # File permissions for custom scripts in airootfs
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/etc/skel/.config/labwc/autostart"]="0:0:755"
   ["/usr/local/bin/advik-menu"]="0:0:755"
